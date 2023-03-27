7f42c678bea78a28bdb29ec9b1c60e1e1b698808
Mateo <6304@holbertonstudents.com>
2023-03-27 11:02:08 -0700

    Progress on printf, created the list and formats given by the task 0

--- _printf.c
+++ _printf.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	ft formats[] = {
+		{ "c", print_c },
+		{ "s", print_s },
+		{ "%", print_mod},
+		{ NULL, NULL }
+	};
+	va_start(args, format);
+}
--- main.h
+++ main.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct format
+{
+	char *form;
+	void (*f)(va_list args);
+} ft;
 int _printf(const char *format, ...);
+void print_c(va_list args);
+void print_s(va_list args);
+void print_mod(va_list args);
 
 #endif
