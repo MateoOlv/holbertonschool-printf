16c211af76d3f5c71c7950ade2130a73f41f2b03
Mateo <6304@holbertonstudents.com>
2023-04-03 05:56:46 -0700

    fix

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct form
+{
+	char *form;
+	int (*f)(va_list args);
+} ft;
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_mod(va_list args);
+int print_int(va_list args);
+#endif
